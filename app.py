d84c508a09a26d9c576ad74922c61aa0bba80574
Tim Glaser <tim.glaser@hiberly.com>
2020-03-10 13:56:40 -0700

    Add cors headers

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from markdown2 import Markdown
 import requests
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
