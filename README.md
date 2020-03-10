b4e75cfc2d756ae5fd1537b8b3a637742e7fe285
Tim Glaser <tim.glaser@hiberly.com>
2020-03-10 12:01:50 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Posthog Update
+
+A super simple microservice that allows PostHog apps to find out if there are any updates.
+
+## Developing locally
+
+```bash
+FLASK_APP=app.py FLASK_DEBUG=1 flask run
+```
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from markdown2 import Markdown
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'PostHog update server'
+
+@app.route('/changelog')
+def changelog():
+    changelog = requests.get('https://raw.githubusercontent.com/PostHog/posthog/master/CHANGELOG.md')
+    markdown = Markdown()
+    return markdown.convert(changelog.text)
+
+@app.route('/latest_version')
+def latest_version():
+    tags = requests.get('https://api.github.com/repos/posthog/posthog/git/refs/tags/')
+    tags = list(reversed(tags.json()))
+    return jsonify([
+        {'tag': tag['ref'].split('/')[-1]} for tag in tags
+    ])
