860b7e60ebd2f52579558b0590f1a30d97f4d531
Tim Glaser <tim.glaser@hiberly.com>
2020-04-10 19:58:19 +0100

    Styling fixes and cache requests

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from markdown2 import Markdown
 import requests
+import requests_cache
 import json
+
+requests_cache.install_cache('github_cache', backend='memory', expire_after=300)
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +18,7 @@ def index():
 def changelog():
     changelog = requests.get('https://raw.githubusercontent.com/PostHog/posthog/master/CHANGELOG.md')
     markdown = Markdown()
-    return render_template('changelog.html', html=markdown.convert(changelog.text).replace("<a", "<a target='_blank'"))
+    return render_template('changelog.html', html=markdown.convert(changelog.text).replace("<a", "<a target='_blank'").replace('<h1>Changelog</h1>', ''))
 
 @app.route('/versions')
 def versions():
